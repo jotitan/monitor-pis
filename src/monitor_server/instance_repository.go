b2ea939d7b28335806a3b9dda92c751130b320f3
Jonathan BARANZINI <titanbar@otmail.com>
2023-11-21 12:55:38 +0100

    FIX some errors

--- src/monitor_server/instance_repository.go
+++ src/monitor_server/instance_repository.go
@@ -17,19 +17,19 @@ const (
 
 // Manage all metrics store
 type MetricRepository struct {
-	folder    string
-	instances map[string]*MetricInstanceRepository
+	folder         string
+	instances      map[string]*MetricInstanceRepository
 	autoFlushLimit int
-	fileCleaner *RetentationProcess
+	fileCleaner    *RetentationProcess
 }
 
-func NewMetricRepository(conf config.MonitoringConfig)*MetricRepository{
+func NewMetricRepository(conf config.MonitoringConfig) *MetricRepository {
 	// Load instances from file
 	mr := &MetricRepository{
-		folder:conf.Folder,
-		instances:make(map[string]*MetricInstanceRepository),
+		folder:         conf.Folder,
+		instances:      make(map[string]*MetricInstanceRepository),
 		autoFlushLimit: conf.AutoFlushLimit,
-		fileCleaner: NewRetentionProcess(conf.RetentionDays,conf.Folder),
+		fileCleaner:    NewRetentionProcess(conf.RetentionDays, conf.Folder),
 	}
 	mr.loadInstancesNames()
 	mr.launchHeartBeats(conf)
@@ -38,40 +38,40 @@ func NewMetricRepository(conf config.MonitoringConfig)*MetricRepository{
 	return mr
 }
 
-func (mr * MetricRepository)launchHeartBeats(conf config.MonitoringConfig){
-	for _,heartbeat :=range conf.GetHeartbeats() {
-		log.Println("Run",heartbeat.Name)
-		heartbeat.Start(mr.getInstance(heartbeatInstanceName,true).Append)
+func (mr *MetricRepository) launchHeartBeats(conf config.MonitoringConfig) {
+	for _, heartbeat := range conf.GetHeartbeats() {
+		log.Println("Run", heartbeat.Name)
+		heartbeat.Start(mr.getInstance(heartbeatInstanceName, true).Append)
 	}
 }
 
-func (mr * MetricRepository)loadInstancesNames(){
-	if data, err := ioutil.ReadFile(mr.getInstancesFilename()) ; err == nil {
-		instances := make([]string,0)
-		json.Unmarshal(data,&instances)
-		for _,instance := range instances {
-			mr.instances[instance] = NewMetricInstanceRepository(mr.folder,instance,nbPointsByBlock,mr.autoFlushLimit)
+func (mr *MetricRepository) loadInstancesNames() {
+	if data, err := ioutil.ReadFile(mr.getInstancesFilename()); err == nil {
+		instances := make([]string, 0)
+		json.Unmarshal(data, &instances)
+		for _, instance := range instances {
+			mr.instances[instance] = NewMetricInstanceRepository(mr.folder, instance, nbPointsByBlock, mr.autoFlushLimit)
 		}
-		log.Println("Load instances",len(mr.instances))
+		log.Println("Load instances", len(mr.instances))
 	}
 }
 
-func (mr * MetricRepository)getInstancesFilename()string{
-	return filepath.Join(mr.folder,"instances.json")
+func (mr *MetricRepository) getInstancesFilename() string {
+	return filepath.Join(mr.folder, "instances.json")
 }
 
-func (mr * MetricRepository)saveInstancesNames(){
-	if f, err := os.OpenFile(mr.getInstancesFilename(),os.O_CREATE|os.O_RDWR|os.O_TRUNC,os.ModePerm); err == nil {
+func (mr *MetricRepository) saveInstancesNames() {
+	if f, err := os.OpenFile(mr.getInstancesFilename(), os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.ModePerm); err == nil {
 		defer f.Close()
-		data,_ := json.Marshal(mr.GetInstancesName())
+		data, _ := json.Marshal(mr.GetInstancesName())
 		f.Write(data)
 	}
 }
 
-func (mr * MetricRepository)getInstance(instanceName string, createIfNoExist bool)*MetricInstanceRepository{
-	instance,exist := mr.instances[instanceName]
+func (mr *MetricRepository) getInstance(instanceName string, createIfNoExist bool) *MetricInstanceRepository {
+	instance, exist := mr.instances[instanceName]
 	if !exist && createIfNoExist {
-		instance = NewMetricInstanceRepository(mr.folder,instanceName,nbPointsByBlock,mr.autoFlushLimit)
+		instance = NewMetricInstanceRepository(mr.folder, instanceName, nbPointsByBlock, mr.autoFlushLimit)
 		mr.instances[instanceName] = instance
 		mr.saveInstancesNames()
 		// Save metrics
@@ -80,60 +80,63 @@ func (mr * MetricRepository)getInstance(instanceName string, createIfNoExist boo
 }
 
 func (mr *MetricRepository) AppendMetrics(instanceName string, metricName string, points []model.MetricPoint) {
-	instance := mr.getInstance(instanceName,true)
-	instance.Append(metricName,points)
+	instance := mr.getInstance(instanceName, true)
+	instance.Append(metricName, points)
 }
 
 func (mr *MetricRepository) AppendManyMetrics(instanceName string, points map[string]model.MetricPoint) {
-	instance := mr.getInstance(instanceName,true)
-	log.Println("Push metric",instanceName)
-	for metricName,value := range points {
-		instance.Append(metricName,[]model.MetricPoint{value})
+	instance := mr.getInstance(instanceName, true)
+	log.Println("Push metric", instanceName)
+	for metricName, value := range points {
+		instance.Append(metricName, []model.MetricPoint{value})
 	}
 }
 
-func (mr *MetricRepository)Search(instanceName, metricName,date string)[]model.MetricPoint{
-	if instance := mr.getInstance(instanceName, false) ; instance != nil {
-		return instance.Search(metricName,date)
+func (mr *MetricRepository) Search(instanceName, metricName, date string) []model.MetricPoint {
+	if instance := mr.getInstance(instanceName, false); instance != nil {
+		return instance.Search(metricName, date)
 	}
 	return []model.MetricPoint{}
 }
 
-func (mr MetricRepository) GetMetricsName(instanceName string)[]string {
-	instance,exist := mr.instances[instanceName]
+func (mr MetricRepository) GetMetricsName(instanceName string) []string {
+	instance, exist := mr.instances[instanceName]
 	if !exist {
 		return []string{}
 	}
 	return instance.getMetricsName()
 }
 
-func (mr MetricRepository) GetLastHeartbeats()map[string]float32 {
-	instance := mr.getInstance(heartbeatInstanceName,false)
+func (mr MetricRepository) GetLastHeartbeats() map[string]float32 {
+	instance := mr.getInstance(heartbeatInstanceName, false)
+	if instance == nil {
+		return map[string]float32{}
+	}
 	return instance.readLastMetrics()
 }
 
-func (mr MetricRepository) GetLastMetrics()map[string]map[string]float32 {
+func (mr MetricRepository) GetLastMetrics() map[string]map[string]float32 {
 	instancesValues := make(map[string]map[string]float32)
-	for _,instanceName := range mr.GetInstancesName() {
+	for _, instanceName := range mr.GetInstancesName() {
 		if !strings.EqualFold(instanceName, heartbeatInstanceName) {
-			instance := mr.getInstance(instanceName,false)
+			instance := mr.getInstance(instanceName, false)
 			instancesValues[instanceName] = instance.readLastMetrics()
 		}
 	}
 	return instancesValues
 }
 
-func (mr MetricRepository) GetInstancesName()[]string {
-	names := make([]string,0,len(mr.instances))
+func (mr MetricRepository) GetInstancesName() []string {
+	names := make([]string, 0, len(mr.instances))
 	for name := range mr.instances {
-		names = append(names,name)
+		names = append(names, name)
 	}
 	return names
 }
 
 func (mr *MetricRepository) Flush() {
 	log.Println("Flush all instances")
-	for _,instance := range mr.instances{
+	for _, instance := range mr.instances {
 		instance.Flush()
 	}
 }
--- src/monitor_server/metric_repository.go
+++ src/monitor_server/metric_repository.go
@@ -19,147 +19,147 @@ const (
 // Header : instance_name(50)-metric_header(20 metrics):name(20)-pointer_start-current_pointer
 
 type headerMetric struct {
-	name string
-	firstBlockPosition int64
-	currentBlockPosition int64	// current
+	name                 string
+	firstBlockPosition   int64
+	currentBlockPosition int64 // current
 }
 
 // Data//| block => nb value (4) | next block position(8)| [timestamp(8) | value (4)]
 type headerFileMetrics struct {
-	name string	// max 48 char
+	name          string // max 48 char
 	headerMetrics []*headerMetric
 }
 
-func newHeader(name string)*headerFileMetrics {
-	return &headerFileMetrics{name: name,headerMetrics:make([]*headerMetric,0,20)}
+func newHeader(name string) *headerFileMetrics {
+	return &headerFileMetrics{name: name, headerMetrics: make([]*headerMetric, 0, 20)}
 }
 
 // structure size name(1) |name (49) | nbMetric(1) | 20[size metric name(1)|name(19)|first block (8) | current block (8)]
-func (h *headerFileMetrics)fromBytes(data []byte){
+func (h *headerFileMetrics) fromBytes(data []byte) {
 	sizeName := int(data[0])
-	h.name = string(data[1:sizeName+1])
+	h.name = string(data[1 : sizeName+1])
 	nbMetrics := int(data[50])
-	position:=51
-	h.headerMetrics = make([]*headerMetric,nbMetrics)
-	for i := 0 ; i < nbMetrics ; i++ {
+	position := 51
+	h.headerMetrics = make([]*headerMetric, nbMetrics)
+	for i := 0; i < nbMetrics; i++ {
 		sizeMetricName := int(data[position])
-		position+=1
-		h.headerMetrics[i] = &headerMetric{name: string(data[position:position+sizeMetricName])}
-		position+=29
-		h.headerMetrics[i].firstBlockPosition = getBytesAsInt64(data[position:position+8])
-		h.headerMetrics[i].currentBlockPosition = getBytesAsInt64(data[position+8:position+16])
-		position+=16
+		position += 1
+		h.headerMetrics[i] = &headerMetric{name: string(data[position : position+sizeMetricName])}
+		position += 29
+		h.headerMetrics[i].firstBlockPosition = getBytesAsInt64(data[position : position+8])
+		h.headerMetrics[i].currentBlockPosition = getBytesAsInt64(data[position+8 : position+16])
+		position += 16
 	}
 }
 
-func(h headerFileMetrics)sizeHeader()int{
+func (h headerFileMetrics) sizeHeader() int {
 	return 971
 }
 
-func (h headerFileMetrics)toBytes()[]byte {
+func (h headerFileMetrics) toBytes() []byte {
 	// Size headerFileMetrics : name(50) + 1 + 20 x (46) = 971
-	data := make([]byte,h.sizeHeader())
+	data := make([]byte, h.sizeHeader())
 	data[0] = byte(len(h.name))
-	writeStringToBytesWithPad(data,h.name,1,49)
+	writeStringToBytesWithPad(data, h.name, 1, 49)
 	data[50] = byte(len(h.headerMetrics))
-	for i,hm := range h.headerMetrics {
-		position := 51+i*46
+	for i, hm := range h.headerMetrics {
+		position := 51 + i*46
 		data[position] = byte(len(hm.name))
-		writeStringToBytesWithPad(data,hm.name,position+1,29)
-		writeBytesToBytes(getInt64AsBytes(hm.firstBlockPosition),data,position+30)
-		writeBytesToBytes(getInt64AsBytes(hm.currentBlockPosition),data,position+38)
+		writeStringToBytesWithPad(data, hm.name, position+1, 29)
+		writeBytesToBytes(getInt64AsBytes(hm.firstBlockPosition), data, position+30)
+		writeBytesToBytes(getInt64AsBytes(hm.currentBlockPosition), data, position+38)
 	}
 	return data
 }
 
-func (h headerFileMetrics)hasMetric(name string)bool{
+func (h headerFileMetrics) hasMetric(name string) bool {
 	return h.getMetric(name) != nil
 }
 
-func (h headerFileMetrics)getMetric(name string)*headerMetric{
-	for _,hm := range h.headerMetrics {
-		if strings.EqualFold(name,hm.name){
+func (h headerFileMetrics) getMetric(name string) *headerMetric {
+	for _, hm := range h.headerMetrics {
+		if strings.EqualFold(name, hm.name) {
 			return hm
 		}
 	}
 	return nil
 }
 
-func (h *headerFileMetrics)createMetric(name string, position int64) (*headerMetric,error){
-	if len(h.headerMetrics) >=20 {
-		return nil,errors.New("impossible to add more than 20 metrics")
+func (h *headerFileMetrics) createMetric(name string, position int64) (*headerMetric, error) {
+	if len(h.headerMetrics) >= 20 {
+		return nil, errors.New("impossible to add more than 20 metrics")
 	}
-	metric := &headerMetric{name:name,currentBlockPosition: position,firstBlockPosition: position}
-	h.headerMetrics = append(h.headerMetrics,metric)
-	return metric,nil
+	metric := &headerMetric{name: name, currentBlockPosition: position, firstBlockPosition: position}
+	h.headerMetrics = append(h.headerMetrics, metric)
+	return metric, nil
 }
 
-func (h *headerFileMetrics)updateMetric(name string, firstBlock, currentClock int64){
+func (h *headerFileMetrics) updateMetric(name string, firstBlock, currentClock int64) {
 	met := h.getMetric(name)
 	met.firstBlockPosition = firstBlock
 	met.currentBlockPosition = currentClock
 }
 
-type blockHeader struct{
-	nbPoints int32
-	nextBlock int64
+type blockHeader struct {
+	nbPoints       int32
+	nextBlock      int64
 	positionInFile int64
-	pointsByBlock int
+	pointsByBlock  int
 }
 
-func createNewBlockHeader(position int64, pointsByBlock int)*blockHeader{
-	return &blockHeader{nbPoints:0,nextBlock:0,positionInFile: position,pointsByBlock:pointsByBlock}
+func createNewBlockHeader(position int64, pointsByBlock int) *blockHeader {
+	return &blockHeader{nbPoints: 0, nextBlock: 0, positionInFile: position, pointsByBlock: pointsByBlock}
 }
 
-func newBlockHeader(f *os.File,position int64,pointsByBlock int)*blockHeader{
-	data := readData(f,position,12)
-	return &blockHeader{nbPoints:getBytesAsInt32(data[0:4]),nextBlock:getBytesAsInt64(data[4:12]),positionInFile: position,pointsByBlock:pointsByBlock}
+func newBlockHeader(f *os.File, position int64, pointsByBlock int) *blockHeader {
+	data := readData(f, position, 12)
+	return &blockHeader{nbPoints: getBytesAsInt32(data[0:4]), nextBlock: getBytesAsInt64(data[4:12]), positionInFile: position, pointsByBlock: pointsByBlock}
 }
 
-func (bh blockHeader)availableSpace()int{
+func (bh blockHeader) availableSpace() int {
 	return bh.pointsByBlock - int(bh.nbPoints)
 }
 
-func (bh blockHeader)readPoints(f * os.File)[]model.MetricPoint{
-	points := make([]model.MetricPoint,bh.nbPoints)
-	data := readData(f,bh.positionInFile+12,int(bh.nbPoints*12))
-	for i := 0 ; i < int(bh.nbPoints) ; i++ {
+func (bh blockHeader) readPoints(f *os.File) []model.MetricPoint {
+	points := make([]model.MetricPoint, bh.nbPoints)
+	data := readData(f, bh.positionInFile+12, int(bh.nbPoints*12))
+	for i := 0; i < int(bh.nbPoints); i++ {
 		points[i] = model.MetricPoint{
-			Timestamp: getBytesAsInt64(data[i*12:i*12+8]),
-			Value:getBytesAsFloat32(data[i*12+8:i*12+12]),
+			Timestamp: getBytesAsInt64(data[i*12 : i*12+8]),
+			Value:     getBytesAsFloat32(data[i*12+8 : i*12+12]),
 		}
 	}
 	return points
 }
 
 // Position : position in file + headerFileMetrics size + points
-func (bh blockHeader)getPositionInBlock()int64{
+func (bh blockHeader) getPositionInBlock() int64 {
 	return bh.positionInFile + 12 + int64(bh.nbPoints)*12
 }
 
-func (bh blockHeader)getSizeBlock()int{
+func (bh blockHeader) getSizeBlock() int {
 	return 12 + bh.pointsByBlock*12
 }
 
-func (bh *blockHeader)updateNbPoints(nbPoints int){
-	bh.nbPoints+=int32(nbPoints)
+func (bh *blockHeader) updateNbPoints(nbPoints int) {
+	bh.nbPoints += int32(nbPoints)
 }
 
-func (bh *blockHeader) flushHeader(f *os.File){
-	f.WriteAt(getInt32AsBytes(bh.nbPoints),bh.positionInFile)
-	f.WriteAt(getInt64AsBytes(bh.nextBlock),bh.positionInFile+4)
+func (bh *blockHeader) flushHeader(f *os.File) {
+	f.WriteAt(getInt32AsBytes(bh.nbPoints), bh.positionInFile)
+	f.WriteAt(getInt64AsBytes(bh.nextBlock), bh.positionInFile+4)
 }
 
 type configRepo struct {
-	name string
-	folder string
+	name           string
+	folder         string
 	autoFlushLImit int
-	pointsByBlock int
+	pointsByBlock  int
 }
 
 type lockers struct {
-	fileLocker * sync.Mutex
-	mapLocker * sync.RWMutex
+	fileLocker *sync.Mutex
+	mapLocker  *sync.RWMutex
 }
 
 // Store metrics of an instance
@@ -168,17 +168,17 @@ type MetricInstanceRepository struct {
 	// Store last values, other values are in file
 	lastMetrics map[string][]model.MetricPoint
 	// Store the headerFileMetrics
-	head *headerFileMetrics
-	locks lockers
- 	metricsName map[string]struct{}
+	head        *headerFileMetrics
+	locks       lockers
+	metricsName map[string]struct{}
 	fileFlusher FileFlusher
 }
 
-func NewMetricInstanceRepository(folder,instanceName string,nbPointsByBlock, autoFlushLimit int)*MetricInstanceRepository{
-	mir :=  &MetricInstanceRepository{
-		conf:configRepo{name: instanceName,folder: folder,pointsByBlock: nbPointsByBlock,autoFlushLImit: autoFlushLimit},
+func NewMetricInstanceRepository(folder, instanceName string, nbPointsByBlock, autoFlushLimit int) *MetricInstanceRepository {
+	mir := &MetricInstanceRepository{
+		conf:        configRepo{name: instanceName, folder: folder, pointsByBlock: nbPointsByBlock, autoFlushLImit: autoFlushLimit},
 		lastMetrics: make(map[string][]model.MetricPoint),
-		locks: lockers{fileLocker: &sync.Mutex{},mapLocker: &sync.RWMutex{}},
+		locks:       lockers{fileLocker: &sync.Mutex{}, mapLocker: &sync.RWMutex{}},
 		metricsName: make(map[string]struct{}),
 		fileFlusher: FileFlusher{nbPointsByBlock},
 	}
@@ -186,14 +186,14 @@ func NewMetricInstanceRepository(folder,instanceName string,nbPointsByBlock, aut
 	return mir
 }
 
-func (r * MetricInstanceRepository)readMetricsNamesFromHeader(){
+func (r *MetricInstanceRepository) readMetricsNamesFromHeader() {
 	filename := r.getFilename(time.Now())
 	// Open file, load headerFileMetrics
-	f,err := os.Open(filename)
+	f, err := os.Open(filename)
 	defer f.Close()
 	if err == nil {
 		r.head = r.readHeader(f)
-		for _,m := range r.head.headerMetrics {
+		for _, m := range r.head.headerMetrics {
 			r.metricsName[m.name] = struct{}{}
 		}
 	}
@@ -210,80 +210,80 @@ func (r MetricInstanceRepository) getMetricsName() []string {
 			set[hm.name] = struct{}{}
 		}
 	}
-	list := make([]string,0,len(set))
+	list := make([]string, 0, len(set))
 	for name := range set {
-		list = append(list,name)
+		list = append(list, name)
 	}
 	return list
 }
 
-func (r * MetricInstanceRepository)Flush()error{
+func (r *MetricInstanceRepository) Flush() error {
 	// Block read/write
 	r.locks.fileLocker.Lock()
 	defer r.locks.fileLocker.Unlock()
 	filename := r.getFilename(time.Now())
 	// Open file, load headerFileMetrics
-	f,err := os.OpenFile(filename,os.O_CREATE|os.O_RDWR,os.ModePerm)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, os.ModePerm)
 	defer f.Close()
 	if err != nil {
 		return err
 	}
 	// Go to end
-	if position,_ := f.Seek(0,2) ; position == 0 {
+	if position, _ := f.Seek(0, 2); position == 0 {
 		// New file, write headerFileMetrics
 		r.head = newHeader(r.conf.name)
 		// Reserve space
-		f.WriteAt(make([]byte,r.head.sizeHeader()),0)
-		r.fileFlusher.flushPoints(r.head,r.locks.mapLocker,f,r.lastMetrics)
-	}else{
+		f.WriteAt(make([]byte, r.head.sizeHeader()), 0)
+		r.fileFlusher.flushPoints(r.head, r.locks.mapLocker, f, r.lastMetrics)
+	} else {
 		if r.head == nil {
 			r.head = r.readHeader(f)
 		}
-		r.fileFlusher.flushPoints(r.head,r.locks.mapLocker,f,r.lastMetrics)
+		r.fileFlusher.flushPoints(r.head, r.locks.mapLocker, f, r.lastMetrics)
 	}
 	r.resetLastMetrics()
 	return nil
 }
 
-func (r * MetricInstanceRepository)resetLastMetrics(){
+func (r *MetricInstanceRepository) resetLastMetrics() {
 	r.locks.mapLocker.Lock()
 	r.lastMetrics = make(map[string][]model.MetricPoint)
 	r.locks.mapLocker.Unlock()
 }
 
-func (r MetricInstanceRepository)readHeader(f *os.File)*headerFileMetrics {
+func (r MetricInstanceRepository) readHeader(f *os.File) *headerFileMetrics {
 	h := newHeader("")
-	h.fromBytes(readData(f,0,h.sizeHeader()))
+	h.fromBytes(readData(f, 0, h.sizeHeader()))
 	return h
 }
 
-func (r MetricInstanceRepository)getFilename(date time.Time)string{
-	return filepath.Join(r.conf.folder,fmt.Sprintf("metric_%s_%s.met",r.conf.name,date.Format("20060102")))
+func (r MetricInstanceRepository) getFilename(date time.Time) string {
+	return filepath.Join(r.conf.folder, fmt.Sprintf("metric_%s_%s.met", r.conf.name, date.Format("20060102")))
 }
 
-func (r * MetricInstanceRepository)Search(metricName,date string)[]model.MetricPoint {
+func (r *MetricInstanceRepository) Search(metricName, date string) []model.MetricPoint {
 	r.locks.mapLocker.RLock()
 	defer r.locks.mapLocker.RUnlock()
 	// Add last metrics only if same date
-	if points,exist := r.lastMetrics[metricName] ; isCurrentDate(date) && exist {
+	if points, exist := r.lastMetrics[metricName]; isCurrentDate(date) && exist {
 		// Read in file and append current points
-		return append(r.readMetricsFromFile(metricName,date),points...)
+		return append(r.readMetricsFromFile(metricName, date), points...)
 	}
-	return r.readMetricsFromFile(metricName,date)
+	return r.readMetricsFromFile(metricName, date)
 }
 
-func isCurrentDate(date string)bool {
-	return strings.EqualFold("",date) || strings.EqualFold(date,time.Now().Format("2006-01-02"))
+func isCurrentDate(date string) bool {
+	return strings.EqualFold("", date) || strings.EqualFold(date, time.Now().Format("2006-01-02"))
 }
 
-func (r *MetricInstanceRepository)readMetricsFromFile(metricName,date string)[]model.MetricPoint{
+func (r *MetricInstanceRepository) readMetricsFromFile(metricName, date string) []model.MetricPoint {
 	readDate := time.Now()
-	if !strings.EqualFold("",date){
-		if d,err := time.Parse("2006-01-02",date) ; err == nil {
+	if !strings.EqualFold("", date) {
+		if d, err := time.Parse("2006-01-02", date); err == nil {
 			readDate = d
 		}
 	}
-	f,_ := os.Open(r.getFilename(readDate))
+	f, _ := os.Open(r.getFilename(readDate))
 	defer f.Close()
 	if r.head == nil {
 		r.head = r.readHeader(f)
@@ -292,69 +292,76 @@ func (r *MetricInstanceRepository)readMetricsFromFile(metricName,date string)[]m
 	if hm == nil {
 		return []model.MetricPoint{}
 	}
-	return r.readBlock(f,hm.firstBlockPosition)
+	return r.readBlock(f, hm.firstBlockPosition)
 }
 
-func (r *MetricInstanceRepository) readLastMetrics()map[string]float32{
-	f,_ := os.Open(r.getFilename(time.Now()))
+func (r *MetricInstanceRepository) readLastMetrics() map[string]float32 {
+	f, err := os.Open(r.getFilename(time.Now()))
+	if err != nil {
+		return map[string]float32{}
+	}
 	defer f.Close()
 	if r.head == nil {
 		r.head = r.readHeader(f)
 	}
 	lasts := make(map[string]float32)
-	for _,name := range r.getMetricsName() {
-		r.readMetricsInFileOfMemory(f,name,lasts)
+	for _, name := range r.getMetricsName() {
+		r.readMetricsInFileOfMemory(f, name, lasts)
 	}
 	return lasts
 }
 
-func (r *MetricInstanceRepository) readMetricsInFileOfMemory(f *os.File,name string,lasts map[string]float32){
+func (r *MetricInstanceRepository) readMetricsInFileOfMemory(f *os.File, name string, lasts map[string]float32) {
 	// Check if last value exist
 	r.locks.mapLocker.RLock()
-	if values,exist := r.lastMetrics[name] ; exist && len(values) > 0{
+	if values, exist := r.lastMetrics[name]; exist && len(values) > 0 {
 		lasts[name] = values[len(values)-1].Value
-	}else{
+	} else {
 		// Read file
 		hm := r.head.getMetric(name)
-		lasts[name] = r.readLastValueBlock(f,hm.currentBlockPosition)
+		if hm != nil {
+			lasts[name] = r.readLastValueBlock(f, hm.currentBlockPosition)
+		} else {
+			lasts[name] = 0
+		}
 	}
 	r.locks.mapLocker.RUnlock()
 }
 
-func (r MetricInstanceRepository)readBlock(f *os.File,position int64)[]model.MetricPoint{
-	bh := newBlockHeader(f,position,r.conf.pointsByBlock)
+func (r MetricInstanceRepository) readBlock(f *os.File, position int64) []model.MetricPoint {
+	bh := newBlockHeader(f, position, r.conf.pointsByBlock)
 	points := bh.readPoints(f)
 	if bh.nextBlock != 0 {
-		points = append(points,r.readBlock(f,bh.nextBlock)...)
+		points = append(points, r.readBlock(f, bh.nextBlock)...)
 	}
 	return points
 }
 
-func (r MetricInstanceRepository)readLastValueBlock(f *os.File,position int64)float32{
-	bh := newBlockHeader(f,position,r.conf.pointsByBlock)
-	lastPosition := bh.positionInFile + int64(12*bh.nbPoints +8)
-	data := readData(f,lastPosition,4)
+func (r MetricInstanceRepository) readLastValueBlock(f *os.File, position int64) float32 {
+	bh := newBlockHeader(f, position, r.conf.pointsByBlock)
+	lastPosition := bh.positionInFile + int64(12*bh.nbPoints+8)
+	data := readData(f, lastPosition, 4)
 	return getBytesAsFloat32(data)
 }
 
-func (r * MetricInstanceRepository)Append(metricName string,points []model.MetricPoint){
+func (r *MetricInstanceRepository) Append(metricName string, points []model.MetricPoint) {
 	r.locks.mapLocker.Lock()
-	metricsPoint,exist := r.lastMetrics[metricName]
+	metricsPoint, exist := r.lastMetrics[metricName]
 	if !exist {
-		metricsPoint = make([]model.MetricPoint,0)
+		metricsPoint = make([]model.MetricPoint, 0)
 		r.lastMetrics[metricName] = metricsPoint
 		r.metricsName[metricName] = struct{}{}
 	}
-	r.lastMetrics[metricName] = append(metricsPoint,points...)
+	r.lastMetrics[metricName] = append(metricsPoint, points...)
 	r.locks.mapLocker.Unlock()
 	r.checkAutoFlush()
 }
 
-func (r * MetricInstanceRepository)checkAutoFlush(){
+func (r *MetricInstanceRepository) checkAutoFlush() {
 	count := 0
 	r.locks.mapLocker.RLock()
-	for _,points := range r.lastMetrics {
-		count+=len(points)
+	for _, points := range r.lastMetrics {
+		count += len(points)
 	}
 	r.locks.mapLocker.RUnlock()
 	if count >= r.conf.autoFlushLImit {
