db7273cc85e24c0e3c66dd105441a58c1d15499c
Jonathan BARANZINI <titanbar@otmail.com>
2026-03-15 21:48:34 +0100

    Correct concurrent map access. Add sync on it

--- src/heartbeat/manager.go
+++ src/heartbeat/manager.go
@@ -1,6 +1,9 @@
 package heartbeat
 
-import "github.com/jotitan/monitor-pis/config"
+import (
+	"github.com/jotitan/monitor-pis/config"
+	"sync"
+)
 
 type Alert struct {
 	email       string
@@ -18,6 +21,7 @@ func newAlert(alert config.AlertConfig, name string) *Alert {
 type Manager struct {
 	// For each url, store number of fail
 	points     map[string]int
+	mapLocker  sync.Mutex
 	alerts     map[string]*Alert
 	mailSender MailSender
 }
@@ -27,6 +31,19 @@ func (m *Manager) RunHeartbeat(hb Heartbeat) bool {
 	hb.manager.recordPoint(success, hb.url)
 
 	return success
+}
+
+func (m *Manager) resetPoint(url string) {
+	m.mapLocker.Lock()
+	m.points[url] = 0
+	m.mapLocker.Unlock()
+}
+
+func (m *Manager) increasePoint(url string) int {
+	m.mapLocker.Lock()
+	m.points[url] = m.points[url] + 1
+	m.mapLocker.Unlock()
+	return m.points[url]
 }
 
 func (m *Manager) recordPoint(success bool, url string) {
@@ -39,16 +56,15 @@ func (m *Manager) recordPoint(success bool, url string) {
 			// Send email cause service is back
 			m.mailSender.sendBackNormal(a.name, url, a.email)
 		}
-		m.points[url] = 0
+		m.resetPoint(url)
 		a.alertSent = false
 	} else {
-		m.points[url] = m.points[url] + 1
-		if a.alertSent == false && m.points[url] >= a.threashold {
+		nb := m.increasePoint(url)
+		if a.alertSent == false && nb >= a.threashold {
 			m.mailSender.sendFail(a.name, url, a.email)
 			a.alertSent = true
 		}
 	}
-
 }
 
 func (m *Manager) RegisterAlert(hb *Heartbeat, alert config.AlertConfig) {
@@ -62,6 +78,7 @@ func (m *Manager) RegisterAlert(hb *Heartbeat, alert config.AlertConfig) {
 func NewManager(emailConfig config.EmailConfig) *Manager {
 	return &Manager{
 		points:     make(map[string]int),
+		mapLocker:  sync.Mutex{},
 		alerts:     make(map[string]*Alert),
 		mailSender: NewMailSender(emailConfig),
 	}
